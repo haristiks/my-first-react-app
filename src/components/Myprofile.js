beddec61e9ca01d6819db6fe93a397e443e507b1
haristiks <nklharis@gmail.com>
2023-07-11 22:44:13 +0530

    first commit

---
+++ src/components/Myprofile.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+function Myprofile() {
+  return (
+    <div className="container">
+      <h1>Hi There! am Haris</h1>
+      <img
+        src="https://haristiks.github.io/portfolio/images/user.jpg"
+        className="profile"
+        alt="haris.jpeg"
+      />
+      <p>
+        Hi, I'm Haris! I'm a self-taught web developer with a passion for
+        building beautiful and functional web applications. I've been learning
+        web development for the past few years, and I'm always eager to learn
+        new things. I'm proficient in HTML, CSS, and JavaScript, and I'm also
+        familiar with other web development technologies, such as React,
+        Node.js, and MySQL.
+      </p>
+      <a href="https://facebook.com/harisnkl1" >
+        facebook
+      </a>
+      <a href="https://twitter.com/haristiks" >
+        twitter
+      </a>
+      <a href="https://instagram.com/haristiks" >
+        instagram
+      </a>
+      <a href="https://github.com/haristiks" >
+        github
+      </a>
+    </div>
+  );
+}
+
+export default Myprofile;
